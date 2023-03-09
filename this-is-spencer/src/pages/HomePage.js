f27cbf5b3f2be209e1c86b40cfec82f3efc71e56
Spencer Robinson <spencerwarmouth@gmail.com>
2023-03-08 19:01:30 -0500

    still designing the top of the home page

--- this-is-spencer/src/pages/HomePage.js
+++ this-is-spencer/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import background from "../imgs/flower.jpg";
 import mistake from "../imgs/redsand.jpg";
+import lightbulb from "../imgs/redLightbulb.jpg";
 import "../css/Home.css";
 import {
   Card,
@@ -12,6 +13,9 @@ import {
   Col,
   CardBody,
   CardFooter,
+  CardSubtitle,
+  Button,
+  img,
 } from "reactstrap";
 
 const HomePage = () => {
@@ -29,7 +33,7 @@ const HomePage = () => {
             <Container fluid className="mt-5">
               <Row>
                 <Col className="extra-bold">
-                  <CardTitle tag="h1" className="extra-bold">
+                  <CardTitle tag="h1" className="extra-bold line-under-blue">
                     Spencer Robinson
                   </CardTitle>
                 </Col>
@@ -42,8 +46,8 @@ const HomePage = () => {
                 </Col>
               </Row> */}
               <Row>
-                <Col xs={{ offset: 1 }}>
-                  <CardTitle tag="h3" className="extra-bold">
+                <Col xs={{ offset: 1 }} className="mt-3">
+                  <CardTitle tag="h3" className="extra-bold line-under-purple">
                     Full Stack Development
                   </CardTitle>
                 </Col>
@@ -59,6 +63,79 @@ const HomePage = () => {
           </CardImgOverlay>
         </Card>
       </div>
+      <Container className="my-5">
+        <Row>
+          <h1 className="text-center my-3 line-under-pink">
+            Successful Full Stack Web and Mobile Development
+          </h1>
+          <h3 className="text-center">
+            Hi. I'm Spencer, a Full Stack Developer with experience building
+            projects from Back End to Front End heres some other words.
+          </h3>
+        </Row>
+        <Row className="my-5">
+          <Col>
+            <Card
+              style={{
+                width: "20rem",
+              }}
+              color="dark"
+              body
+            >
+              <img alt="Sample" src={lightbulb} />
+              <CardBody>
+                <CardTitle tag="h5" className="text-center">
+                  MERN Stack Development
+                </CardTitle>
+                <CardText className="text-center">
+                  Responsive websites built for an optimal user experience that
+                  achieves your business goals.
+                </CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card
+              style={{
+                width: "20rem",
+              }}
+              color="dark"
+              body
+            >
+              <img alt="Sample" src={lightbulb} rounded />
+              <CardBody>
+                <CardTitle tag="h5" className="text-center">
+                  MERN Stack Development
+                </CardTitle>
+                <CardText>
+                  Responsive websites built for an optimal user experience that
+                  achieves your business goals.
+                </CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card
+              style={{
+                width: "20rem",
+              }}
+              color="dark"
+              body
+            >
+              <img alt="Sample" src={lightbulb} />
+              <CardBody>
+                <CardTitle tag="h5" className="text-center">
+                  MERN Stack Development
+                </CardTitle>
+                <CardText>
+                  Responsive websites built for an optimal user experience that
+                  achieves your business goals.
+                </CardText>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
